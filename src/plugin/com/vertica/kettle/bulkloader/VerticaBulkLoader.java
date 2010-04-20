01b33659c11c91d13de05d8fef23affde7fcff0e
Daniel Einspanjer <deinspanjer@mozilla.com>
2010-04-20 20:02:53 +0000

    Remove obsolete run method from plugins to match changes in PDI 4.0

--- src/plugin/com/vertica/kettle/bulkloader/VerticaBulkLoader.java
+++ src/plugin/com/vertica/kettle/bulkloader/VerticaBulkLoader.java
@@ -443,11 +443,4 @@ public class VerticaBulkLoader extends BaseStep implements StepInterface
         }
         super.dispose(smi, sdi);
     }
-
-    //
-    // Run is were the action happens!
-    public void run()
-    {
-        BaseStep.runStepThread(this, meta, data);
-    }
 }
